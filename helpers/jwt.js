2e21d0e9c1a114e07e593d743a920e3345f866f1
NimeshBhevan <nimeshbhevan.ar@gmail.com>
2024-08-13 18:19:06 +0530

    refractoring

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -14,34 +14,3 @@ function authJWT() {
 
 module.exports = authJWT();
 
-
-// var { expressjwt: jwt } = require("express-jwt");
-
-// function authJWT() {
-// 	const secret = process.env.JWT_SECRET;
-// 	return jwt({
-// 		secret,
-// 		algorithms: ["HS256"],
-// 		getToken: (req) => {
-// 			if (
-// 				req.headers.authorization &&
-// 				req.headers.authorization.split(" ")[0] === "Bearer"
-// 			) {
-// 				return req.headers.authorization.split(" ")[1];
-// 			}
-// 			return null;
-// 		},
-// 	}).unless({
-// 		path: ["/api/users/register", "/api/users/login"],
-// 	});
-// }
-
-// Error handling middleware
-// function errorHandler(err, req, res, next) {
-// 	if (err.name === "UnauthorizedError") {
-// 		return res.status(401).send({ message: "Invalid Token" });
-// 	}
-// 	next(err);
-// }
-
-// module.exports = { authJWT, errorHandler };
--- routes/todo.js
+++ routes/todo.js
@@ -19,19 +19,13 @@ router.post("/", authJWT, async (req, res) => {
 
 // GET with the userID - api/todo/
 router.get("/", authJWT, async (req, res) => {
-	// console.log(`user: ${req.user.userId}`)
 	let tasks = await ToDo.find({ userId: req.user.userId });
-	// console.log(typeof (req.user.userId));
 	res.send(tasks);
 });
 
 // DELETE - api/todo/:id
 router.delete("/:id", authJWT, async (req, res) => {
 	try {
-		// console.log(`id: ${req.params.id}`);
-		// console.log(`user: ${req.user.userId}`);
-		// console.log(typeof req.params.id);
-		// console.log(typeof req.user.userId);
 		console.log(req.user);
 		let task = await ToDo.findOneAndDelete({
 			_id: req.params.id,
@@ -51,10 +45,6 @@ router.delete("/:id", authJWT, async (req, res) => {
 // PUT - api/todo/:id
 router.put("/:id", authJWT, async (req, res) => {
 	try {
-		console.log(`${req.params.id}`);
-		console.log(`${req.user.userId}`);
-		console.log(typeof req.params.id);
-		console.log(typeof req.user.userId);
 		const task = await ToDo.findOneAndUpdate(
 			{ _id: req.params.id, userId: req.user.userId },
 			{
@@ -77,37 +67,3 @@ router.put("/:id", authJWT, async (req, res) => {
 });
 
 module.exports = router;
-
-// //get - api/todo
-// router.get("/", async (req, res) => {
-// 	let tasks = await ToDo.find();
-// 	res.send(tasks);
-// });
-
-// //delete - api/todo/:id
-// router.delete("/:id", async (req, res) => {
-// 	let task = await ToDo.findByIdAndDelete(req.params.id);
-// 	if (!task) {
-// 		return res.status(404).send("Task not found");
-// 	}
-// 	res.send(task);
-// });
-
-// // Put - api/todo/:id
-// router.put("/:id", async (req, res) => {
-// 	const task = await ToDo.findByIdAndUpdate(
-// 		req.params.id,
-// 		{
-// 			title: req.body.title,
-// 			description: req.body.description,
-// 			date: req.body.date,
-// 			done: req.body.done,
-// 		},
-// 		{ new: true }
-// 	);
-
-// 	if (!task) {
-// 		return res.status(404).send("Task cannot be updated");
-// 	}
-// 	res.send(task);
-// });
