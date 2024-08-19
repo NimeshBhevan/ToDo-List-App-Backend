7be18bc226264bebf38421ad8c44642653049883
NimeshBhevan <nimeshbhevan.ar@gmail.com>
2024-08-19 12:10:28 +0530

    modified .env

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,5 +1,6 @@
 var { expressjwt: jwt } = require("express-jwt");
 
+// this function helps in deconstructing the jwt token for authenticating the user 
 function authJWT() {
 	const secret = process.env.JWT_SECRET;
 	return jwt({
@@ -7,7 +8,9 @@ function authJWT() {
 		algorithms: ["HS256"],
 		requestProperty: 'user'
 		
-	}).unless({
+	})
+		// .unless() method is used to exclude certain paths from authentication
+		.unless({
 		path: ["/api/users/register", `/api/users/login`],
 	});
 }
